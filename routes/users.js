e0d0f01f86ea66d9098dd263c5f35066bcbe24f7
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-01-22 23:11:22 +0200

    fixes

--- routes/users.js
+++ routes/users.js
@@ -95,6 +95,7 @@ router.post('/register',
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(password, salt, (err, hash) => {
             if(err) throw err;
+            console.log(`Hashed password: ${hash}`);
             User.create(
               {
                 username: username,
