18cbecadb24c56d44ff575f60bd88d23aea61f7c
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-01-22 20:20:58 +0200

    Added secondary register and login routes

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Secondary register route
+router.get('/register.html', (req, res) => {
+  res.render('register');
+});
+
 // Register route
 router.get('/register', (req, res) => {
   res.render('register');
+});
+
+// Secondary login route
+router.get('/login.html', (req, res) => {
+  res.render('login');
 });
 
 // Login route
 router.get('/login', (req, res) => {
   res.render('login');
 });
+
+
 
 // Export router
 module.exports = router;
